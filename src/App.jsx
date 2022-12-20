4a044563c541f7b0f20fdea9ab064d446014c176
Prakhar <ptofficial29@gmail.com>
2022-12-20 14:57:49 +0530

    table slice created, basic ui components made, column visiblity toggle feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react'
+import AttributeIcon from './components/AttributeIcon';
+import IconComponent from './components/IconComponent';
+import Button from './components/Button';
+import { ColumnToggleFeature } from './features/table/ColumnToggleFeature';
 
 function App() {
 
   return (
-    <div className="bg-blue-200 text-xl w-full text-center">
-      SetUp Done.
+    <div className="w-full text-center">
+      <div className="bg-white">
+      <header className="w-full bg-blue-300 h-fit p-3 font-bold text-md sm:text-xl text-left">
+        Analytics
+      </header>
+      <ColumnToggleFeature />
+
+      <IconComponent text="TEST" onClick={()=>console.log("hello")}/>
+      <AttributeIcon attributeName="TEST" isVisible={true} onClick={()=>console.log("hello")}/>
+      <AttributeIcon attributeName="TEST" isVisible={false} onClick={()=>console.log("hello")}/>
+      <Button text="Apply" fill={true}  onClick={()=>console.log("hello")} />
+      <Button text="Apply" fill={false}  onClick={()=>console.log("hello")} />
+    </div>
     </div>
   )
 }
---
+++ src/features/table/ColumnToggleFeature.jsx
@@ -0,0 +1,30 @@
+// Toggle visibilty of the column
+// reorder it as well
+
+import React from 'react'
+import { changeVisibility } from './tableSlice'
+import { useSelector,useDispatch } from 'react-redux'
+import AttributeIcon from '../../components/AttributeIcon'
+
+export const ColumnToggleFeature = () => {
+    const dispatch = useDispatch()
+    const columns = useSelector((state)=>state.table.attributes)
+    console.log("DATA RECIEVED",columns)
+  return (
+    <div className='flex p-5 m-2 border-2 rounded transition'>
+        {
+            columns.map((col)=>{
+                return <AttributeIcon 
+                attributeName={col.nameOfAttribute} 
+                isVisible={col.visible} 
+                onClick={()=>{
+                    dispatch(changeVisibility({
+                        nameOfAttribute:col.nameOfAttribute,
+                        visible:!col.visible,
+                    }))
+                }}/>
+            })
+        }
+    </div>
+  )
+}
