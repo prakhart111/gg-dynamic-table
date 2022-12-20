9c45b8b5f7d81fbfbdfcf9b988a291d31b678926
Prakhar <ptofficial29@gmail.com>
2022-12-20 14:12:54 +0530

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="bg-blue-200 text-xl w-full text-center">
+      SetUp Done.
+    </div>
+  )
+}
+
+export default App
---
+++ src/features/table/tableSlice.js
@@ -0,0 +1,1 @@
+const BASE_URL = process.env.BASE_URL
